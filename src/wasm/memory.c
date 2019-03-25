4858afea9d89364f3077133e4f3794fc4d146183
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-03-24 20:36:40 -0500

    Blind implementation of memory

---
+++ src/wasm/memory.c
@@ -0,0 +1,29 @@
+#include "memory.h"
+
+uint8_t fdm_get(fd_memory* m, uint16_t addr) {
+  if (addr < 0x8000) {
+    return m->cartridge[addr];
+  } else if (addr < 0xA000) {
+    return m->vram[addr - 0x8000];
+  } else if (addr < 0xC000) {
+    return 0; // switchable RAM bank
+  } else if (addr < 0xE000) {
+    return m->ram[addr - 0xC000];
+  } else if (addr < 0xFE00) {
+    return m->ram[addr - 0xE000]; // Echo of 8kB Internal RAM
+  } else if (addr < 0xFEA0) {
+    return m->oam[addr - 0xFE00];
+  } else if (addr < 0xFF00) {
+    return 0; // Empty but unusable for I/O
+  } else if (addr < 0xFF4C) {
+    return 0; // I/O ports
+  } else if (addr < 0xFF80) {
+    return 0; // Empty but unusable for I/O
+  } else if (addr < 0xFFFF) {
+    return 0; // Internal RAM
+  } else if (addr == 0xFFFF) {
+    return 0; // Interrupt enable register
+  }
+
+  return UINT8_MAX;
+}
---
+++ src/wasm/memory.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+#define KB 1024
+
+typedef struct {
+  uint8_t cartridge[32 * KB];
+  uint8_t ram[8 * KB];
+  uint8_t vram[8 * KB];
+  uint8_t oam[40 * 4];
+} fd_memory;
+
+uint8_t fdm_get(fd_memory* m, uint16_t addr);
