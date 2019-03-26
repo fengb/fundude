18daf57f9d0a7457a0815af90b7621fdc9040f4b
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-03-25 23:44:03 -0500

    Convert to instr datatype

--- src/wasm/cpu.c
+++ src/wasm/cpu.c
@@ -7,6 +7,13 @@ typedef struct {
   fd_memory mem;
 } fd_cpu;
 
+typedef struct {
+  int length;
+  int duration;
+} instr;
+
+#define INSTR(length, duration) ((instr){ length, duration })
+
 uint16_t w2(uint8_t op[]) {
   return (op[1] << 8) + op[2];
 }
@@ -18,8 +25,6 @@ bool will_overflow(uint8_t a, uint8_t b) {
 bool will_underflow(uint8_t a, uint8_t b) {
   return a < b;
 }
-
-typedef int cycles;
 
 /* These functions are a lot more scannable if the names line up, hence the cryptic abbreviations:
      rr -- register (8bit)
@@ -33,70 +38,70 @@ typedef int cycles;
      1F -- double-byte address (16bit)
 */
 
-cycles op_nop() {
-  return 1;
+instr op_nop() {
+  return INSTR(1, 4);
 }
 
-cycles op_rlc_rr___(fd_cpu* _, reg8* tgt) {
+instr op_rlc_rr___(fd_cpu* _, reg8* tgt) {
   int msb = tgt->_ >> 7 & 1;
   tgt->_ = tgt->_ << 1 | msb;
-  return 1;
+  return INSTR(1, 4);
 }
 
-cycles op_lod_rr_08(fd_cpu* _, reg8* tgt, uint8_t d8) {
+instr op_lod_rr_08(fd_cpu* _, reg8* tgt, uint8_t d8) {
   tgt->_ = d8;
-  return 2;
+  return INSTR(2, 8);
 }
 
-cycles op_lod_ww_16(fd_cpu* _, reg16* tgt, uint16_t d16) {
+instr op_lod_ww_16(fd_cpu* _, reg16* tgt, uint16_t d16) {
   tgt->_ = d16;
-  return 3;
+  return INSTR(3, 12);
 }
 
-cycles op_lod_WW_rr(fd_cpu* cpu, reg16* tgt, reg8* src) {
+instr op_lod_WW_rr(fd_cpu* cpu, reg16* tgt, reg8* src) {
   fdm_set(&cpu->mem, tgt->_, src->_);
-  return 2;
+  return INSTR(1, 8);
 }
 
-cycles op_lod_1F_ww(fd_cpu* cpu, uint16_t a16, reg16* src) {
+instr op_lod_1F_ww(fd_cpu* cpu, uint16_t a16, reg16* src) {
   fdm_set(&cpu->mem, a16, src->_);
-  return 3;
+  return INSTR(3, 20);
 }
 
-cycles op_inc_ww___(fd_cpu* _, reg16* tgt) {
+instr op_inc_ww___(fd_cpu* _, reg16* tgt) {
   tgt->_++;
-  return 2;
+  return INSTR(1, 8);
 }
 
-cycles op_add_rr_08(fd_cpu* cpu, reg8* tgt, uint8_t val) {
+instr op_add_rr_08(fd_cpu* cpu, reg8* tgt, uint8_t val) {
   fd_flags f = fd_get_flags(&cpu->reg);
   f.N = false;
   f.H = will_overflow(tgt->_, val);
   tgt->_ += val;
   f.Z = tgt->_ == 0;
   fd_set_flags(&cpu->reg, f);
-  return 1;
+  return INSTR(1, 4);
 }
 
-cycles op_sub_rr_08(fd_cpu* cpu, reg8* tgt, uint8_t val) {
+instr op_sub_rr_08(fd_cpu* cpu, reg8* tgt, uint8_t val) {
   fd_flags f = fd_get_flags(&cpu->reg);
   f.N = true;
   f.H = will_underflow(tgt->_, val);
   tgt->_ -= val;
   f.Z = tgt->_ == 0;
   fd_set_flags(&cpu->reg, f);
-  return 1;
+  return INSTR(1, 4);
 }
 
-cycles op_inc_rr___(fd_cpu* cpu, reg8* tgt) {
+instr op_inc_rr___(fd_cpu* cpu, reg8* tgt) {
   return op_add_rr_08(cpu, tgt, 1);
 }
 
-cycles op_dec_rr___(fd_cpu* cpu, reg8* tgt) {
+instr op_dec_rr___(fd_cpu* cpu, reg8* tgt) {
   return op_sub_rr_08(cpu, tgt, 1);
 }
 
-cycles run(fd_cpu* cpu, uint8_t op[]) {
+instr run(fd_cpu* cpu, uint8_t op[]) {
   switch (op[0]) {
     case 0x00: return op_nop();
     case 0x01: return op_lod_ww_16(cpu, &cpu->reg.BC, w2(op));
@@ -107,20 +112,22 @@ cycles run(fd_cpu* cpu, uint8_t op[]) {
     case 0x06: return op_lod_rr_08(cpu, &cpu->reg.B, op[1]);
     case 0x07: return op_rlc_rr___(cpu, &cpu->reg.A);
     case 0x08: return op_lod_1F_ww(cpu, w2(op), &cpu->reg.SP);
-    case 0x09: return 0;
-    case 0x0A: return 0;
-    case 0x0B: return 0;
-    case 0x0C: return 0;
-    case 0x0D: return 0;
-    case 0x0E: return 0;
-    case 0x0F: return 0;
+    case 0x09: return INSTR(0, 0);
+    case 0x0A: return INSTR(0, 0);
+    case 0x0B: return INSTR(0, 0);
+    case 0x0C: return INSTR(0, 0);
+    case 0x0D: return INSTR(0, 0);
+    case 0x0E: return INSTR(0, 0);
+    case 0x0F: return INSTR(0, 0);
   }
 
   assert(false); // Op not implemented
-  return 0;
+  return INSTR(0, 0);
 }
 
 void tick(fd_cpu* cpu) {
-  cycles c = run(cpu, fdm_ptr(&cpu->mem, cpu->reg.PC._));
-  cpu->reg.PC._ += c;
+  instr c = run(cpu, fdm_ptr(&cpu->mem, cpu->reg.PC._));
+  assert(c.length > 0);
+  assert(c.duration > 0);
+  cpu->reg.PC._ += c.length;
 }
