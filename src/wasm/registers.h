c9f9eadf9d9df4e621ae5387037db49e802d2a4f
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-03-27 08:21:01 -0500

    Resolve little-endian

--- src/wasm/registers.h
+++ src/wasm/registers.h
@@ -1,9 +1,19 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define ENDIAN(def1, def2) \
+    def1; \
+    def2;
+#else
+#define ENDIAN(def1, def2) \
+    def2; \
+    def1;
+#endif
+
 typedef struct {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-  unsigned int _padding : 4;
+  uint8_t _padding : 4;
   bool C : 1;
   bool H : 1;
   bool N : 1;
@@ -13,7 +23,7 @@ typedef struct {
   bool N : 1;
   bool H : 1;
   bool C : 1;
-  unsigned int _padding : 4;
+  uint8_t _padding : 4;
 #endif
 } fd_flags;
 
@@ -26,6 +36,42 @@ typedef struct {
 } reg16;
 
 typedef struct {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+  union {
+    reg16 AF;
+    struct {
+      union {
+        reg8 F;
+        fd_flags FLAGS;
+      };
+      reg8 A;
+    };
+  };
+
+  union {
+    reg16 BC;
+    struct {
+      reg8 C;
+      reg8 B;
+    };
+  };
+
+  union {
+    reg16 DE;
+    struct {
+      reg8 E;
+      reg8 D;
+    };
+  };
+
+  union {
+    reg16 HL;
+    struct {
+      reg8 L;
+      reg8 H;
+    };
+  };
+#else
   union {
     reg16 AF;
     struct {
@@ -60,6 +106,7 @@ typedef struct {
       reg8 L;
     };
   };
+#endif
 
   reg16 SP;
   reg16 PC;
