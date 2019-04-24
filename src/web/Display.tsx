0539c0328d03daa2de3bfe08f71248ba83df9c78
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-04-24 08:55:01 -0500

    Render helper grid

--- src/web/Display.tsx
+++ src/web/Display.tsx
@@ -1,4 +1,16 @@
 import React from "react";
+import { style } from "typestyle";
+
+const CSS = {
+  root: style({
+    display: "block",
+    backgroundSize: "8px 8px",
+    backgroundImage: [
+      "linear-gradient(to right, lightgray 2px, transparent 1px)",
+      "linear-gradient(to bottom, lightgray 2px, transparent 1px)"
+    ].join(",")
+  })
+};
 
 const PADDING = 1;
 
@@ -9,8 +21,13 @@ const PALETTE: Record<number, Uint8Array> = {
   3: Uint8Array.of(15, 56, 15, 255)
 };
 
-function imageData(pixels: Uint8Array, width: number) {
-  const imageData = new ImageData(width, pixels.length / width);
+interface Matrix extends Uint8Array {
+  width: number;
+  height: number;
+}
+
+function imageData(pixels: Matrix) {
+  const imageData = new ImageData(pixels.width, pixels.height);
   for (let i = 0; i < pixels.length; i++) {
     const colorIndex = pixels[i];
     const color = PALETTE[colorIndex] || Uint8Array.of(255, 0, 0, 255);
@@ -19,12 +36,7 @@ function imageData(pixels: Uint8Array, width: number) {
   return imageData;
 }
 
-interface Matrix extends Uint8Array {
-  width: number;
-  height: number;
-}
-
-export default function Display(props: { pixels: Matrix }) {
+export default function Display(props: { pixels: Matrix; scale?: number }) {
   const ref = React.useRef<HTMLCanvasElement>(null);
   React.useEffect(() => {
     if (!ref.current) {
@@ -32,15 +44,12 @@ export default function Display(props: { pixels: Matrix }) {
     }
 
     const ctx = ref.current.getContext("2d")!;
-    ctx.putImageData(
-      imageData(props.pixels, props.pixels.width),
-      PADDING,
-      PADDING
-    );
+    ctx.putImageData(imageData(props.pixels), PADDING, PADDING);
   });
 
   return (
     <canvas
+      className={CSS.root}
       ref={ref}
       width={props.pixels.width + PADDING * 2}
       height={props.pixels.height + PADDING * 2}
