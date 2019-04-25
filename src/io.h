2b9668929b345fd500d751e21d078d34c72e5249
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-04-24 21:52:27 -0500

    Comment the IO registers

--- src/io.h
+++ src/io.h
@@ -37,43 +37,43 @@ typedef union {
         uint8_t _padding : 2;
       };
     } P1;
-    uint8_t SB;
-    uint8_t SC;
-    uint8_t _pad_ff04;
-    uint8_t DIV;
-    uint8_t TIMA;
-    uint8_t TMA;
-    uint8_t TAC;
-    uint8_t _pad_ff07_ef[7];
-    uint8_t IF;
+    uint8_t SB;  // $FF01
+    uint8_t SC;  // $FF02
+    uint8_t _pad_ff03;
+    uint8_t DIV;   // $FF04
+    uint8_t TIMA;  // $FF05
+    uint8_t TMA;   // $FF06
+    uint8_t TAC;   // $FF07
+    uint8_t _pad_ff08_0e[7];
+    uint8_t IF;  // FF0F
 
-    uint8_t NR10;
-    uint8_t NR11;
-    uint8_t NR12;
-    uint8_t NR13;
-    uint8_t NR14;
-    uint8_t NR21;
-    uint8_t NR22;
-    uint8_t NR23;
-    uint8_t NR24;
+    uint8_t NR10;  // $FF10
+    uint8_t NR11;  // $FF11
+    uint8_t NR12;  // $FF12
+    uint8_t NR13;  // $FF13
+    uint8_t NR14;  // $FF14
+    uint8_t NR21;  // $FF15
+    uint8_t NR22;  // $FF16
+    uint8_t NR23;  // $FF17
+    uint8_t NR24;  // $FF18
     uint8_t _pad_ff1a;
-    uint8_t NR30;
-    uint8_t NR31;
-    uint8_t NR32;
-    uint8_t NR33;
-    uint8_t NR34;
+    uint8_t NR30;  // $FF1A
+    uint8_t NR31;  // $FF1B
+    uint8_t NR32;  // $FF1C
+    uint8_t NR33;  // $FF1D
+    uint8_t NR34;  // $FF1E
     uint8_t _pad_ff1f;
 
-    uint8_t NR41;
-    uint8_t NR42;
-    uint8_t NR43;
-    uint8_t NR44;
-    uint8_t NR50;
-    uint8_t NR51;
-    uint8_t NR52;
+    uint8_t NR41;  // $FF20
+    uint8_t NR42;  // $FF21
+    uint8_t NR43;  // $FF22
+    uint8_t NR44;  // $FF23
+    uint8_t NR50;  // $FF24
+    uint8_t NR51;  // $FF25
+    uint8_t NR52;  // $FF26
     uint8_t _pad_ff27_2f[9];
 
-    uint8_t wave_pattern[0x10];
+    uint8_t wave_pattern[0x10];  // $FF30 - FF3F
 
     struct {
       bool bg_enable : 1;
@@ -93,15 +93,15 @@ typedef union {
       bool oam_int : 1;
       bool coincidence_int : 1;
     } STAT;
-    uint8_t SCY;
-    uint8_t SCX;
-    uint8_t LY;
-    uint8_t LYC;
-    uint8_t DMA;
-    color_palette BGP;
-    color_palette OBP0;
-    color_palette OBP1;
-    uint8_t WY;
-    uint8_t WX;
+    uint8_t SCY;         // $FF42
+    uint8_t SCX;         // $FF43
+    uint8_t LY;          // $FF44
+    uint8_t LYC;         // $FF45
+    uint8_t DMA;         // $FF46
+    color_palette BGP;   // $FF47
+    color_palette OBP0;  // $FF48
+    color_palette OBP1;  // $FF49
+    uint8_t WY;          // $FF4A
+    uint8_t WX;          // $FF4B
   };
 } fd_io;
