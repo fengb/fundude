75822be7ab4b1ffb0b43b87f98b73953e972f663
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-04-26 23:26:54 -0500

    Fix memory test

--- test/test_memory_ptr.c
+++ test/test_memory_ptr.c
@@ -1,16 +1,21 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include "memory.h"
 #include "tap_eq.h"
 
 int main() {
-  plan(10);
+  plan(11);
 
   fd_memory mem;
+  mem.boot_complete = false;
+  eqptr(fdm_ptr(&mem, 0x0000), BOOTLOADER);
+
+  mem.boot_complete = true;
   eqptr(fdm_ptr(&mem, 0x0000), mem.cart);
   eqptr(fdm_ptr(&mem, 0x8000), &mem.vram);
   eqptr(fdm_ptr(&mem, 0xA000), mem.switchable_ram);
   eqptr(fdm_ptr(&mem, 0xC000), mem.ram);
-  eqptr(fdm_ptr(&mem, 0xE000), mem.ram); // echo of RAM
+  eqptr(fdm_ptr(&mem, 0xE000), mem.ram);  // echo of RAM
   eqptr(fdm_ptr(&mem, 0xFE00), mem.oam);
   eqptr(fdm_ptr(&mem, 0xFF00), mem.io_ports.RAW);
   eqptr(fdm_ptr(&mem, 0xFF50), &mem.boot_complete);
