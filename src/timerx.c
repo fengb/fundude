56d4700736fa9f5510e37a63f075a0e567d637ac
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-05-03 20:30:24 -0500

    Fix? timer logic

--- src/timerx.c
+++ src/timerx.c
@@ -1,32 +1,31 @@
 #include "timerx.h"
 
-static int tima_shift(io_timer_speed t) {
+static uint8_t tima_shift(io_timer_speed t, uint8_t cycles) {
   switch (t) {
-    case IO_TIMER_SPEED_4096: return 1024;  // 2 ** 10
-    case IO_TIMER_SPEED_16384: return 256;  // 2 ** 8
-    case IO_TIMER_SPEED_65536: return 64;   // 2 ** 6
-    case IO_TIMER_SPEED_262144: return 16;  // 2 ** 4
-    default: return INT16_MAX;
+    case IO_TIMER_SPEED_4096: return cycles * 256 / 1024;  // every 1024 cycles
+    case IO_TIMER_SPEED_16384: return cycles * 256 / 256;  // every 256 cycles
+    case IO_TIMER_SPEED_65536: return cycles * 256 / 64;   // every 64 cycles
+    case IO_TIMER_SPEED_262144: return cycles * 256 / 16;  // every 16 cycles
+    default: return 1;
   }
 }
 
-static uint16_t step_up(int shift, int base, int add) {
-  return ((base + add) >> shift) - (base >> shift);
-}
-
 void timer_step(fundude* fd, uint8_t cycles) {
+  fd->clock.timer += cycles;  // overflow is fine
+  fd->mmu.io_ports.DIV = fd->clock.timer / 256;
+
   if (!fd->mmu.io_ports.TAC.active) {
     return;
   }
 
   uint8_t start = fd->mmu.io_ports.TIMA;
-  fd->mmu.io_ports.TIMA += step_up(tima_shift(fd->mmu.io_ports.TAC.speed), fd->clock.timer, cycles);
+
+  fd->mmu.io_ports.TIMA += tima_shift(fd->mmu.io_ports.TAC.speed, cycles);
+
+  // if overflowed
   if (fd->mmu.io_ports.TIMA < start) {
     // TODO: this effect actually happen 1 cycle later
     fd->mmu.io_ports.TIMA += fd->mmu.io_ports.TMA;
-    fd->mmu.io_ports.IF.vblank = true;
+    fd->mmu.io_ports.IF.timer = true;
   }
-
-  fd->clock.timer += cycles;  // overflow is fine
-  fd->mmu.io_ports.DIV = fd->clock.timer / 256;
 }
