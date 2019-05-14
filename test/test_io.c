44c6bb72d7ce5603f827336447425e6c3bde244f
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-05-14 18:45:17 -0500

    Pull out apu_io

--- test/test_io.c
+++ test/test_io.c
@@ -3,23 +3,21 @@
 #include "tap_eq.h"
 
 int main() {
-  plan(12);
+  plan(13);
 
   eqhex(sizeof(io), 0x4C);
 
   eqhex(offsetof(io, timer), 0x04);
   eqhex(offsetof(io, IF), 0x0F);
 
-  eqhex(offsetof(io, NR10), 0x10);
-  eqhex(offsetof(io, NR34), 0x1E);
-
-  eqhex(offsetof(io, NR41), 0x20);
-  eqhex(offsetof(io, NR52), 0x26);
-
-  eqhex(offsetof(io, wave_pattern), 0x30);
+  eqhex(offsetof(io, apu), 0x10);
+  eqhex(offsetof(apu_io, NR10), 0x00);
+  eqhex(offsetof(apu_io, NR34), 0x0E);
+  eqhex(offsetof(apu_io, NR41), 0x10);
+  eqhex(offsetof(apu_io, NR52), 0x16);
+  eqhex(offsetof(apu_io, wave_pattern), 0x20);
 
   eqhex(offsetof(io, ppu), 0x40);
-
   eqhex(offsetof(ppu_io, LCDC), 0x0);
   eqhex(offsetof(ppu_io, OBP1), 0x9);
   eqhex(offsetof(ppu_io, WY), 0xA);
