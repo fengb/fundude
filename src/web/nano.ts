6b48ba4dba2f37be64c9b2f6f2f3dccbed17ec21
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-08-12 21:20:21 -0500

    Properly hydrate styles

--- src/web/nano.ts
+++ src/web/nano.ts
@@ -1,15 +1,19 @@
-import { h } from "preact";
 import { create } from "nano-css";
 
 import { addon as addonRule } from "nano-css/addon/rule";
 import { addon as addonCache } from "nano-css/addon/cache";
+import { addon as addonHydrate } from "nano-css/addon/hydrate";
 import { addon as addonNesting } from "nano-css/addon/nesting";
 import { CssLikeObject } from "nano-css/types/common";
 
-const nano = create({ h });
+const nano = create({
+  pfx: "fd",
+  sh: document.getElementById("nano-css") as any
+});
 
 addonRule(nano);
 addonCache(nano);
+addonHydrate(nano);
 addonNesting(nano);
 
 export default Object.assign(nano, {
