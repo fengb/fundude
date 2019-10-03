5b442521d31e06cca4563604004e6e4d6217e9fd
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-10-03 14:57:02 -0500

    preactX stable

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -2,6 +2,6 @@ import "preact/debug";
 import { h, render, hydrate } from "preact";
 import Page from "./Page";
 
-const container = document.body;
+const container = document.getElementById('app');
 const r = container.hasChildNodes() ? hydrate : render;
 r(h(Page), container);
