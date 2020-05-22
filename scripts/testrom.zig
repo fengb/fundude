0df0f962e10511655c58b7ef2301fe754e5e3764
Benjamin Feng <benjamin.feng@glassdoor.com>
2020-05-21 21:26:24 -0500

    Support multiple roms at once

--- scripts/testrom.zig
+++ scripts/testrom.zig
@@ -3,41 +3,44 @@ const Fundude = @import("fundude");
 
 pub fn main() !u8 {
     const args = try std.process.argsAlloc(std.heap.page_allocator);
-    if (args.len != 2) {
+    if (args.len < 2) {
         std.debug.warn("Usage: {} [path/to/test-rom.gb]\n", .{"testrom"});
         return 1;
     }
 
-    var cart: [0x8000]u8 = undefined;
+    var status: u8 = 0;
+    for (args[1..]) |arg| {
+        var cart: [0x8000]u8 = undefined;
 
-    const cwd = std.fs.cwd();
-    const file = try cwd.openFile(args[1], .{});
-    defer file.close();
+        const cwd = std.fs.cwd();
+        const file = try cwd.openFile(arg, .{});
+        defer file.close();
 
-    const red = try file.readAll(&cart);
-    if (red != cart.len) {
-        std.debug.warn("Cart length {} != {}", .{ red, cart.len });
-        return 1;
-    }
+        const red = try file.readAll(&cart);
+        if (red != cart.len) {
+            std.debug.warn("Cart length {} != {}", .{ red, cart.len });
+            return 1;
+        }
 
-    var fd: Fundude = undefined;
-    try fd.load(&cart);
-    while (fd.cpu.reg._16.get(.PC) < 0x7FFD) {
-        _ = fd.step();
-    }
+        var fd: Fundude = undefined;
+        try fd.load(&cart);
+        while (fd.cpu.reg._16.get(.PC) < 0x7FFD) {
+            _ = fd.step();
+        }
 
-    const stack_top = fd.cpu.reg._16.get(.SP);
-    const expecteds = cart[0x4000..];
-    const actuals = std.mem.asBytes(&fd.mmu.dyn)[stack_top..0xE000];
+        const stack_top = fd.cpu.reg._16.get(.SP);
+        const expecteds = cart[0x4000..];
+        const actuals = std.mem.asBytes(&fd.mmu.dyn)[stack_top..0xE000];
 
-    std.debug.warn("Result length: 0x{X}\n", .{actuals.len});
+        std.debug.warn("{} compared bytes: 0x{X}\n", .{ arg, actuals.len });
 
-    var result: u8 = 0;
-    for (actuals) |actual, i| {
-        if (actual != expecteds[i]) {
-            std.debug.warn("0x{X}: 0x{X} != 0x{X} \n", .{ stack_top + i, actual, expecteds[i] });
+        for (actuals) |actual, i| {
+            if (actual != expecteds[i]) {
+                std.debug.warn("0x{X}: 0x{X} != 0x{X} \n", .{ stack_top + i, actual, expecteds[i] });
+                status = 1;
+            }
         }
     }
 
-    return result;
+    return status;
 }
